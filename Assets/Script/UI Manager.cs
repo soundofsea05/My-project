12b6a6a4b1c186eb96360bee4235af05af7a40a1
mimiduqu <145083878+soundofsea05@users.noreply.github.com>
2026-07-07 21:41:56 +0900

    update 1 file and create 75 files

---
+++ Assets/Script/UI Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text lifeText;
+    public TMP_Text scoreText;
+
+    public int life = 3;
+    public int score = 0;
+
+    void Update()
+    {
+        lifeText.text = "Life : " + life;
+        scoreText.text = "Score : " + score;
+        
+    }
+}
