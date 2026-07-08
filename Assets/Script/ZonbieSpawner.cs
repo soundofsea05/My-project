afe54c9a6f9e1b709f5da3c7f2bb00b3be3dc7b7
mimiduqu <145083878+soundofsea05@users.noreply.github.com>
2026-07-08 21:17:15 +0900

    rename 4 files, copy 66 files, move and rename 23 files, update 2 files, create 232 files, move 3 files and delete 96 files

--- Assets/Script/ZonbieSpawner.cs
+++ Assets/Script/ZonbieSpawner.cs
@@ -13,6 +13,7 @@ public class ZonbieSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float angle = Random.Range(0f, 360f);
+        Vector3 pos = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * 20,0,Mathf.Sin(angle * Mathf.Deg2Rad) * 20);
     }
 }
